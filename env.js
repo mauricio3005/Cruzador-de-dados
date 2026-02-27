831e89dab1d3fb8febc198c8d1df44024abddd77
mauricio3005 <mauriciomenezeslemos@gmail.com>
2026-02-27 19:17:30 -0300

    Delete env.js

--- env.js
+++
@@ -1,4 +0,0 @@
-window.ENV = {
-    SUPABASE_URL: "https://gjqjblhfbmbvmnkqykms.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqcWpibGhmYm1idm1ua3F5a21zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjUxOTMsImV4cCI6MjA4NzcwMTE5M30.b7_FBAMf514nuyEuFWpivhCMkh7_SjF5rwloQT4QgJQ"
-};
